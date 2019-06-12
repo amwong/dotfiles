facbb6dda861ac0ef80bf720e8f1e1066edf56db
Adelia Wong <wadelia@amazon.com>
2019-06-12 12:17:06 -0500

    merged in changes and made small updates

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@
 mv ../dotfiles $HOME/.dotfiles
 
 sudo apt-get install zsh
-ln -s  ~/.dotfiles/zsh/.zshenv $HOME/.zshenv 
-ln -s ~/.dotfiles/zsh/.zshrc $HOME/.zshrc
+ln -s  ~/.dotfiles/zsh/zshenv $HOME/.zshenv 
+ln -s ~/.dotfiles/zsh/zshrc $HOME/.zshrc
 
 sudo apt-get install vim
-ln -s ~/.dotfiles/vim/.vimrc $HOME/.vimrc
+ln -s ~/.dotfiles/vim/vimrc $HOME/.vimrc
 
 zsh
