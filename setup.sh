4a0408fabd967cbad13e870cdef4918f2f3004b9
Adelia Wong <wadelia@amazon.com>
2021-04-28 13:14:38 -0600

    added ada aliases

--- setup.sh
+++ setup.sh
@@ -2,11 +2,9 @@
 
 mv ../dotfiles $HOME/.dotfiles
 
-sudo apt-get install zsh
 ln -s  ~/.dotfiles/zsh/zshenv $HOME/.zshenv 
 ln -s ~/.dotfiles/zsh/zshrc $HOME/.zshrc
 
-sudo apt-get install vim
 ln -s ~/.dotfiles/vim/vimrc $HOME/.vimrc
 
 zsh
